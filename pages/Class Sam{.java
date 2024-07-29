d7110b5a0b7b65529a2d4918b2499b9c24b6f4ef
sanjh17 <sanjanasmathpati@gmail.com>
2024-07-29 14:01:31 +0530

    project push

---
+++ pages/Class Sam{.java
@@ -0,0 +1,17 @@
+
+public class  Sam{ 
+    
+    abstract class Language {
+        public void display(){
+            System.out.println("This is Java Programming");
+        }
+    }
+
+      class main extends Language {
+          public  static void main (String [] args){
+              Main obj = new Main ();
+              obj.display();
+          }
+      }
+    
+}
